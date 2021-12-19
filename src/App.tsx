9ef042b050f5d1bf5db6f2bb02060e466cb31f92
MRadzynski <radzynski.milosz@gmail.com>
2021-12-19 12:51:42 +0100

    Added react-router-dom 6, setup routes and added pages for each route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+
+import About from './page/About';
+import Contact from './page/Contact';
+import Gallery from './page/Gallery';
+import Offer from './page/Offer';
+import Pricing from './page/Pricing';
+import Homepage from './page/Homepage';
+
 import './App.css';
 
-function App() {
-	return <div>Hello World!</div>;
-}
+const App = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Homepage />} />
+			<Route path="/o-nas" element={<About />} />
+			<Route path="/galeria" element={<Gallery />} />
+			<Route path="/oferta" element={<Offer />} />
+			<Route path="/cennik" element={<Pricing />} />
+			<Route path="/kontakt" element={<Contact />} />
+		</Routes>
+	);
+};
 
 export default App;
---
+++ src/page/Offer.tsx
@@ -0,0 +1,5 @@
+const Offer = () => {
+	return <p>Offer</p>;
+};
+
+export default Offer;
