33822cbcd42c137d74149bdd882fd78a550e5c48
MRadzynski <radzynski.milosz@gmail.com>
2022-01-06 13:01:57 +0100

    Added background image and action button to Homepage

--- src/page/Homepage.tsx
+++ src/page/Homepage.tsx
@@ -1,5 +1,20 @@
+import ActionButton from '../components/Action-button/Action-button';
+
+import classes from '../styles/components/homepage.module.scss';
+
 const Homepage = () => {
-	return <p>Home</p>;
+	return (
+		<section className={classes.homepageContainer}>
+			<div className={classes.heroImageContainer}>
+				<img
+					alt="Woman with cream mask"
+					className={classes.heroImage}
+					src="/assets/images/hero.jpg"
+				/>
+			</div>
+			<ActionButton />
+		</section>
+	);
 };
 
 export default Homepage;
